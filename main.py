ab14ed5594616dc1bb80f8c68d89bde8e988cb20
Farrukhy <159027406+Farrukhy@users.noreply.github.com>
2024-09-23 20:49:31 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#This file will be the main script to execute the project. It imports the necessary parts and runs them.
+
+# main.py
+
+# Import the preprocessing, model training, and evaluation files
+import rc.preprocessing
+import rc.model_training
+import rc.evaluation
